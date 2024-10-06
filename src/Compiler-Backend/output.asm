9d2410305e8fd2a5c24c878c87acfc158da70569
RajDaBest <imagination12344321.com@gmail.com>
2024-10-07 03:03:33 +0530

    minor bug fixing

--- src/Compiler-Backend/output.asm
+++ src/Compiler-Backend/output.asm
@@ -1,23 +1,27 @@
 section .bss
 stack: resq 1024
 section .data
-stack_top: dq stack
+stack_top: dq (stack + 8192)
 
 section .text
 global _start
 _start:
 mov rsi, [stack_top]
-add rsi, 8
+sub rsi, 8
 mov QWORD [rsi], 10
 mov [stack_top], rsi
 mov rsi, [stack_top]
-add rsi, 8
+sub rsi, 8
 mov QWORD [rsi], 18
 mov [stack_top], rsi
 mov rsi, [stack_top]
 mov rax, [rsi]
-sub rsi, 8
+add rsi, 8
 add [rsi], rax
+mov [stack_top], rsi
+mov rsi, [stack_top]
+sub rsi, 8
+mov QWORD [rsi], 90
 mov [stack_top], rsi
 mov rbx, [stack_top]
 mov rax, 60
--- src/Compiler-Backend/vasm2amd64.c
+++ src/Compiler-Backend/vasm2amd64.c
@@ -70,7 +70,8 @@ bool init_compiler_context(CompilerContext *ctx, const char *output_file)
         return false;
     }
 
-    fprintf(ctx->data_file, "section .bss\nstack: resq %zu\nsection .data\nstack_top: dq stack\n", vm_stack_capacity);
+    size_t offset = vm_stack_capacity * sizeof(uint64_t);
+    fprintf(ctx->data_file, "section .bss\nstack: resq %zu\nsection .data\nstack_top: dq (stack + %zu)\n", vm_stack_capacity, offset);
     fprintf(ctx->program_file, "section .text\nglobal _start\n");
 
     ctx->code_section_offset = 0;
@@ -187,13 +188,13 @@ bool handle_instruction(CompilerContext *ctx, size_t inst_number, String_View *o
     {
     case INST_UPUSH:
     case INST_SPUSH:
-        fprintf(ctx->program_file, "mov rsi, [stack_top]\nadd rsi, 8\nmov QWORD [rsi], %.*s\nmov [stack_top], rsi\n",
+        fprintf(ctx->program_file, "mov rsi, [stack_top]\nsub rsi, 8\nmov QWORD [rsi], %.*s\nmov [stack_top], rsi\n",
                 (int)operand->count, operand->data);
         break;
     case INST_FPUSH:
         fprintf(ctx->data_file, "L%zu: dq %.*s\n", ctx->l_num,
                 (int)operand->count, operand->data);
-        fprintf(ctx->program_file, "mov rsi, [stack_top]\nmovsd xmm0, [L%zu]\nadd rsi, 8\nmovsd [rsi], xmm0\nmov [stack_top], rsi\n",
+        fprintf(ctx->program_file, "mov rsi, [stack_top]\nsub rsi, 8\nmovsd xmm0, [L%zu]\nmovsd [rsi], xmm0\nmov [stack_top], rsi\n",
                 ctx->l_num);
         ctx->l_num++;
         break;
@@ -202,10 +203,10 @@ bool handle_instruction(CompilerContext *ctx, size_t inst_number, String_View *o
         break;
     case INST_SPLUS:
     case INST_UPLUS:
-        fprintf(ctx->program_file, "mov rsi, [stack_top]\nmov rax, [rsi]\nsub rsi, 8\nadd [rsi], rax\nmov [stack_top], rsi\n");  // stack is the pointer to the stack; stack_top is the pointer to the address of stack top
+        fprintf(ctx->program_file, "mov rsi, [stack_top]\nmov rax, [rsi]\nadd rsi, 8\nadd [rsi], rax\nmov [stack_top], rsi\n"); // stack is the pointer to the stack; stack_top is the pointer to the address of stack top
         break;
     case INST_FPLUS:
-        fprintf(ctx->program_file, "mov rsi, [stack_top]\nmovsd xmm0, [rsi]\nsub rsi, 8\nvaddsd xmm0, [rsi]\nvmovsd [rsi], xmm0\nmov [stack_top], rsi\n");
+        fprintf(ctx->program_file, "mov rsi, [stack_top]\nmovsd xmm0, [rsi]\nadd rsi, 8\nvaddsd xmm0, [rsi]\nvmovsd [rsi], xmm0\nmov [stack_top], rsi\n");
         break;
 
     default:
